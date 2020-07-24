4ccd020add81bca046ecd0921ff060e18172cf66
wangxiaoyang <15847424609@163.com>
2020-07-24 22:37:47 +0800

    添加独立的登录页面

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-card>
+        <div slot="header">
+            <div class="title">注册</div>
+        </div>
+        <Login/>
+    </el-card>
+</template>
+
+<script>
+    import Login from '~/components/Login';
+
+    export default {
+        name: 'login',
+        layout: 'blog',
+        components: {
+            Login
+        }
+    }
+</script>
+<style scoped>
+    .el-card {
+        max-width: 360px;
+        border-radius: 10px;
+        margin: 6.25rem auto auto;
+        padding: 0.625rem 0.625rem 0;
+    }
+
+    .el-button {
+        width: 100%;
+    }
+
+    .title {
+        font-weight: 600;
+        text-align: center;
+        font-size: 1.125rem;
+    }
+</style>
